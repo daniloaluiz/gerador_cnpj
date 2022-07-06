683a0daf5caa2d65ab9c6c7c863dce2cff6bb768
Hideraldo <hideraldus13@gmail.com>
2022-07-06 18:33:16 -0300

    criado objeto cnpj com geracao de dv ao instanciar

---
+++ cnpj.py
@@ -0,0 +1,36 @@
+from logging import raiseExceptions
+
+class Cnpj:
+
+    def __valid_cnpj_length__(self, cnpj):
+        try:
+            if len(str(cnpj)) == 8:
+                return True
+        except:
+            pass
+
+        return False
+
+    def __gera_dv__(self, cnpj):
+        cnpj = list(map(int,cnpj)) + [0, 0, 0, 1]
+        for _ in range(2):
+            value = sum(v * (i % 8 + 2) for i, v in enumerate(reversed(cnpj)))
+            digit = 11 - value % 11
+            cnpj.append(digit if digit < 10 else 0)
+        return "".join(str(x) for x in cnpj[12:14])
+
+    def __init__(self, cnpj):
+        if self.__valid_cnpj_length__(cnpj):
+            self.cnpj_basico = cnpj
+            self.ordem = '0001'
+            self.dv = self.__gera_dv__(str(cnpj))
+            self.cnpj_formatado = self.__cnpj_formatado__() 
+        else:
+            raise Exception("O formato do CNPJ informado é inválido") 
+
+    def __str__(self):
+        return str(self.cnpj_basico) + self.ordem + str(self.dv)
+    
+    def __cnpj_formatado__(self):
+        cnpj = str(self.cnpj_basico)
+        return cnpj[:2] +"."+ cnpj[2:5] +"."+ cnpj[5:] +'/'+ self.ordem +'-'+ str(self.dv)
--- gerador.py
+++ gerador.py
@@ -1,27 +1,26 @@
 from datetime import datetime
+from cnpj import Cnpj
 
-LENGTH_CNPJ = 14
 INITIAL_CNPJ = 46208711
-RANGE =100
+QUANTITY = 100000
+
+DIR_OUTPUT = 'C:\\Dev\\'
 COMA_SEPARATOR = ','
 
-def completa_cnpj(cnpj):
-    cnpj = list(map(int,cnpj)) + [0, 0, 0, 1]
-    for _ in range(2):
-        value = sum(v * (i % 8 + 2) for i, v in enumerate(reversed(cnpj)))
-        digit = 11 - value % 11
-        cnpj.append(digit if digit < 10 else 0)
-    return "".join(str(x) for x in cnpj)
-
 if __name__ == "__main__":
+   
     print('Começando em: '+str(datetime.now().time().strftime('%H:%M:%S')))
-    with open('cnpjs.csv','w') as f:
+    
+    with open(DIR_OUTPUT+'cnpjs.csv','w') as f:
         f.write('cnpj_basico,cnpj_ordem,cnpj_dv,cnpj\n')
-        for i in range(RANGE):
-            cnpj =completa_cnpj(str(INITIAL_CNPJ))
-            cnpj_dv = cnpj[12:14]
-            f.write(str(INITIAL_CNPJ)+ COMA_SEPARATOR +'0001' +COMA_SEPARATOR+cnpj_dv+COMA_SEPARATOR+cnpj+'\n')
-            INITIAL_CNPJ +=1
+        next_cnpj = INITIAL_CNPJ
+
+        for i in range(QUANTITY):
+            print(str(i+1) +' / '+ str(QUANTITY), end='\r')
+            new_cnpj = Cnpj(str(next_cnpj))
+            f.write(new_cnpj.cnpj_basico + COMA_SEPARATOR + new_cnpj.ordem + COMA_SEPARATOR + new_cnpj.dv + COMA_SEPARATOR + str(new_cnpj) +'\n')
+            next_cnpj +=1
+
+        print(str(i+1) +' / '+ str(QUANTITY))        
 
     print('Terminando em: '+str(datetime.now().time().strftime('%H:%M:%S')))
-
