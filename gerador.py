d720554bcc27685d4e63e701ec69b6fbc582cb5f
Felipe Pereira <fdiaspp@gmail.com>
2022-07-06 23:10:02 -0300

    Melhorando legibilidade de codigo.

--- gerador.py
+++ gerador.py
@@ -1,8 +1,14 @@
+import os
 import argparse
 from datetime import datetime
 
 from cnpj import Cnpj
 
+
+OUTPUT_FILE_NAME = 'cnpjs.csv'
+
+def print_count_log(count, total_count, end_line="\n"):
+    print(f"{count}/{total_count}", end=end_line)
 
 if __name__ == "__main__":
 
@@ -31,21 +37,39 @@ if __name__ == "__main__":
     args = parser.parse_args()    
     initial_cnpj = args.initial_cnpj
     dir_output = args.output_path
-    cvs_sep = args.sep_csv
+    csv_sep = args.sep_csv
     quantity_cnpj_to_be_generated = args.quantity
 
     print('Começando em: '+str(datetime.now().time().strftime('%H:%M:%S')))
 
-    with open(dir_output+'cnpjs.csv','w') as f:
-        f.write('cnpj_basico,cnpj_ordem,cnpj_dv,cnpj\n')
+    output_file_path = os.path.join(dir_output, OUTPUT_FILE_NAME)
+    csv_header = 'cnpj_basico,cnpj_ordem,cnpj_dv,cnpj'
+
+    with open(output_file_path,'w') as f:
+        f.write(csv_header)
         next_cnpj = initial_cnpj
 
         for i in range(quantity_cnpj_to_be_generated):
-            print(str(i+1) +' / '+ str(quantity_cnpj_to_be_generated), end='\r')
-            new_cnpj = Cnpj(str(next_cnpj))
-            f.write(new_cnpj.cnpj_basico + cvs_sep + new_cnpj.ordem + cvs_sep + new_cnpj.dv + cvs_sep + str(new_cnpj) +'\n')
-            next_cnpj +=1
+            print_count_log(
+                count=i+1,
+                total_count=quantity_cnpj_to_be_generated,
+                end_line='\r'
+            )
 
-        print(str(i+1) +' / '+ str(quantity_cnpj_to_be_generated))        
+            new_cnpj = Cnpj(str(next_cnpj))
+            row_content = csv_sep.join([
+                new_cnpj.cnpj_basico,
+                new_cnpj.ordem,
+                new_cnpj.dv,
+                str(new_cnpj)
+            ])
+
+            f.write('\n' + row_content)
+            next_cnpj +=1
+            
+        print_count_log(
+            count=i+1,
+            total_count=quantity_cnpj_to_be_generated
+        )       
 
     print('Terminando em: '+str(datetime.now().time().strftime('%H:%M:%S')))
